1a9e83438a17fa131bae4a6cad43ef822d63f9b5
Lauri1989 <147425507+Lauri1989@users.noreply.github.com>
2024-03-13 15:43:41 +0200

    Viimased täiustused

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import raamat1 from './img/raamat1.jpg';
+import raamat2 from './img/raamat2.jpg';
+import raamat3 from './img/raamat3.jpg';
+import './Books.css';
+import BooksList from './BooksList';
+import Raamat from './Raamat';
+
+const books = [
+  {title: 'Tee mehe südamesse', author: 'Margus Vaher', image: raamat1},
+  {title: 'MIDAGI TÕELIST', author: 'MARTIN ALGUS', image: raamat2},
+  {title: 'KUU TEINE POOL', author: 'Urmas Vadi', image: raamat3},
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="container">
+      <div className="books-list">
+        <BooksList books={books} />
+      </div>
+      <div className="book-details">
+        {books.map((book) => {
+          return (
+            <Raamat title={book.title} author={book.author} image={book.image} />
+          );
+        })
+        }
+      </div>
     </div>
   );
 }
